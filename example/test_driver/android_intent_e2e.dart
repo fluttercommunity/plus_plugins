5172a752c306de02f4913ed296d78622923966e5
Miguel Beltran <m@beltran.work>
2020-07-19 21:26:38 +0200

    Fix e2e tests (#9)
    Commentary: I don't understand why originally the test was set to
    isFalse instead of isTrue when the title of the test says that it was
    expected to be true. Besides, the expectLater call didn't work as
    it was implemented.
    
    The implementation in this commit fixes it and now all the 5 e2e test
    pass.

--- example/test_driver/android_intent_e2e.dart
+++ example/test_driver/android_intent_e2e.dart
@@ -56,12 +56,12 @@ void main() {
       package: 'io.flutter.plugins.androidintentexample',
       componentName: 'io.flutter.embedding.android.FlutterActivity',
     );
-    await expectLater(() async => await intent.canResolveActivity(), isFalse);
+    await expectLater(await intent.canResolveActivity(), isTrue);
   }, skip: !Platform.isAndroid);
 
   testWidgets('#canResolveActivity returns false when no Activity is found',
       (WidgetTester tester) async {
     const intent = AndroidIntent(action: 'LAUNCH', package: 'foobar');
-    await expectLater(() async => await intent.canResolveActivity(), isFalse);
+    await expectLater(await intent.canResolveActivity(), isFalse);
   }, skip: !Platform.isAndroid);
 }
