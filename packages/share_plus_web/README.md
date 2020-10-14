ef9432d5629ce6c500d21e9182ddd3a7d31d7f4b
balvinderz <balvindersi2@gmail.com>
2020-10-14 17:16:13 +0530

    introduce share_plus_web

---
+++ packages/share_plus_web/README.md
@@ -0,0 +1,6 @@
+# Share Plus Web
+
+[![Flutter Community: sensors_plus_web](https://fluttercommunity.dev/_github/header/share_plus_web)](https://github.com/fluttercommunity/community)
+
+
+The Web implementation of [`share_plus`](https://pub.dev/packages/share_plus).
---
+++ packages/share_plus_web/lib/share_plus_web.dart
@@ -0,0 +1,39 @@
+import 'dart:html' as html;
+import 'dart:ui';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:share_plus_platform_interface/share_plus_platform_interface.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+/// The web implementation of [SharePlatform].
+class SharePlugin extends SharePlatform {
+  /// Registers this class as the default instance of [SharePlatform].
+  static void registerWith(Registrar registrar) {
+    SharePlatform.instance = SharePlatform();
+  }
+
+  final _navigator = html.window.navigator;
+
+  Future<void> share(
+    String text, {
+    String subject,
+    Rect sharePositionOrigin,
+  }) async {
+    try {
+      await _navigator.share({'title': subject, 'text': text});
+    } catch (e) {
+      //Navigator is not available or the webPage is not served on https
+      final uri = Uri.encodeFull('mailto:?subject=$subject&body=$text');
+      return launch(uri);
+    }
+  }
+
+  @override
+  Future<void> shareFiles(List<String> paths,
+      {List<String> mimeTypes,
+      String subject,
+      String text,
+      Rect sharePositionOrigin}) {
+    throw UnimplementedError('shareFiles() has not been implemented on Web.');
+  }
+}
