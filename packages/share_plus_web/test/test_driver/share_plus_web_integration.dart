b391e6e162418f2371dd66b09be010437c384ce3
balvinderz <balvindersi2@gmail.com>
2020-10-14 21:09:43 +0530

    added integration test

--- packages/share_plus_web/test/test_driver/share_plus_web_integration.dart
+++ packages/share_plus_web/test/test_driver/share_plus_web_integration.dart
@@ -5,12 +5,13 @@ import 'dart:html' as html;
 
 import 'package:share_plus_web/share_plus_web.dart';
 
-
 class _MockWindow extends Mock implements html.Window {}
+
 class _MockNavigator extends Mock implements html.Navigator {}
+
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  group('SharePlugin',(){
+  group('SharePlugin', () {
     _MockWindow mockWindow;
     _MockNavigator mockNavigator;
 
@@ -22,6 +23,14 @@ void main() {
       when(mockWindow.navigator).thenReturn(mockNavigator);
 
       plugin = SharePlugin(debugNavigator: mockNavigator);
+    });
+    group('share', () {
+      testWidgets('can share url', (WidgetTester _) async {
+        expect(plugin.share('https://google.com'), completes);
+      });
+      testWidgets('can share text with subject', (WidgetTester _) async {
+        expect(plugin.share('flutter', subject: 'test'), completes);
+      });
     });
   });
 }
