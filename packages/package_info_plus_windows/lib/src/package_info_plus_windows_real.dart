cf13721d4daf7b838fff6b7442f41a8a67b9fd34
J-P Nurmi <jpnurmi@gmail.com>
2020-11-07 21:30:25 +0100

    [package_info_plus_windows] move FFI behind a conditional import
    Same as for path_provider_windows:
    https://github.com/flutter/plugins/commit/25957033132ac17094e82bec58e429fac073a483
    
    Moves the real implementation of package_info_plus_windows behind a
    conditional export, instead exporting a stub on platforms that don't
    support dart:ffi. This avoids build breakage in web projects that have
    transitive dependencies on package_info_plus (and thus
    package_info_plus__windows due to manual endorsement).
    
    This will no longer be necessary once
    https://github.com/flutter/flutter/issues/52267 is fixed, since only
    Windows builds will ever need to have code-level dependency on
    package_info_plus_windows.

---
+++ packages/package_info_plus_windows/lib/src/package_info_plus_windows_real.dart
@@ -0,0 +1,36 @@
+/// The Windows implementation of `package_info_plus`.
+library package_info_plus_windows;
+
+import 'dart:io';
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+import 'package:package_info_plus_platform_interface/package_info_data.dart';
+import 'package:package_info_plus_platform_interface/package_info_platform_interface.dart';
+import 'package:win32/win32.dart';
+
+part 'file_version_info.dart';
+
+/// The Windows implementation of [PackageInfoPlatform].
+class PackageInfoWindows extends PackageInfoPlatform {
+  /// Returns a map with the following keys:
+  /// appName, packageName, version, buildNumber
+  @override
+  Future<PackageInfoData> getAll() {
+    final info = _FileVersionInfo(Platform.resolvedExecutable);
+    final versions = info.productVersion.split('+');
+    final data = PackageInfoData(
+      appName: info.productName,
+      packageName: info.internalName,
+      version: versions.getOrNull(0),
+      buildNumber: versions.getOrNull(1),
+    );
+    info.dispose();
+    return Future.value(data);
+  }
+}
+
+extension _GetOrNull<T> on List<T> {
+  T getOrNull(int index) => _checkIndex(index) ? this[index] : null;
+  bool _checkIndex(int index) => index >= 0 && index < length;
+}
---
+++ packages/package_info_plus_windows/lib/src/package_info_plus_windows_stub.dart
@@ -0,0 +1,21 @@
+import 'package:package_info_plus_platform_interface/package_info_data.dart';
+import 'package:package_info_plus_platform_interface/package_info_platform_interface.dart';
+
+/// A stub implementation to satisfy compilation of multi-platform packages that
+/// depend on package_info_plus_windows. This should never actually be created.
+///
+/// Notably, because package_info_plus needs to manually register
+/// package_info_plus_windows, anything with a transitive dependency on
+/// package_info_plus will also depend on package_info_plus_windows, not just at
+/// the pubspec level but the code level.
+class PackageInfoWindows extends PackageInfoPlatform {
+  /// Errors on attempted instantiation of the stub. It exists only to satisfy
+  /// compile-time dependencies, and should never actually be created.
+  PackageInfoWindows() {
+    assert(false);
+  }
+
+  /// Stub
+  @override
+  Future<PackageInfoData> getAll() => null;
+}
