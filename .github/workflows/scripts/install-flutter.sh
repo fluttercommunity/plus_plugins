82e1ec7a4373b81a60b5c52ae982af662e38625f
Miguel Beltran <m@beltran.work>
2020-12-05 16:08:41 +0100

    set beta in install script

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -6,7 +6,7 @@ if [ "$BRANCH" == "dev" ]
 then
   # TODO Flutter dev branch is currently broken so we're unable to test MacOS.
   echo "TODO: Skipping macOS testing due to Flutter dev branch issue. Switching branch to stable."
-  BRANCH=stable
+  BRANCH=beta
 fi
 
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
