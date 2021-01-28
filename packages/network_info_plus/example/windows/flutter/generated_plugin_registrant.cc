89fc91f9ca19e92bb19453a056c23d35d6206e04
J-P Nurmi <jpnurmi@gmail.com>
2021-01-28 10:07:40 +0100

    [network_info_plus] add Windows support (#87)

---
+++ packages/network_info_plus/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <network_info_plus_windows/network_info_plus_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  NetworkInfoPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("NetworkInfoPlusWindowsPlugin"));
+}
