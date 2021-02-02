248420d5455a300d56e2bc41b1e5b31f4c84df34
Miguel Beltran <m@beltran.work>
2021-02-02 09:28:25 +0100

    migrated plugin

--- packages/android_intent_plus/test/android_intent_test.dart
+++ packages/android_intent_plus/test/android_intent_test.dart
@@ -11,7 +11,7 @@ import 'package:platform/platform.dart';
 
 void main() {
   AndroidIntent androidIntent;
-  MockMethodChannel mockChannel;
+  late MockMethodChannel mockChannel;
   setUp(() {
     mockChannel = MockMethodChannel();
   });
