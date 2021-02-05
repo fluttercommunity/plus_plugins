7a8006d6f294f48ba6d8a7294d4098c7a8568e01
J-P Nurmi <jpnurmi@gmail.com>
2021-02-05 17:39:47 +0100

    [network_info_plus_web] add stub implementation
    Throws unsupported exceptions, which is nicer and more descriptive than
    unclear missing method channel exceptions without the stub.

---
+++ packages/network_info_plus_web/lib/network_info_plus_web.dart
@@ -0,0 +1,26 @@
+import 'package:network_info_plus_platform_interface/network_info_plus_platform_interface.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+/// A stub implementation of the NetworkInfoPlatform interface for Web.
+class NetworkInfoPlusPlugin extends NetworkInfoPlatform {
+  /// Factory method that initializes the network info plugin platform with
+  /// an instance of the plugin for the web.
+  static void registerWith(Registrar registrar) {
+    NetworkInfoPlatform.instance = NetworkInfoPlusPlugin();
+  }
+
+  /// Obtains the wifi name (SSID) of the connected network
+  Future<String> getWifiName() {
+    throw UnsupportedError('getWifiName() is not supported on Web.');
+  }
+
+  /// Obtains the wifi BSSID of the connected network.
+  Future<String> getWifiBSSID() {
+    throw UnsupportedError('getWifiBSSID() is not supported on Web.');
+  }
+
+  /// Obtains the IP address of the connected wifi network
+  Future<String> getWifiIP() {
+    throw UnsupportedError('getWifiIP() is not supported on Web.');
+  }
+}
