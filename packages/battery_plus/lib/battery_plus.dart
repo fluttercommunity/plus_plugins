fa38995efb91831044ca173a8c97b1c44397470b
pr_Mais <hiimechi@gmail.com>
2021-02-17 00:18:12 +0300

    [battery_plus] address pub.dev score (#16)  (#140)
    * [battery_plus] relocate method_channel_foo.dart (#16)
    
    Importing `platform_interface/src/foo.dart` from the main package loses
    10 pub score points:
    
        Pass static analysis
    
        20/30 points: code has no errors, warnings, lints, or formatting issues
    
        INFO: Don't import implementation files from another package.
    
    * [battery_plus] bump versions

--- packages/battery_plus/lib/battery_plus.dart
+++ packages/battery_plus/lib/battery_plus.dart
@@ -7,7 +7,7 @@ import 'dart:io' show Platform;
 
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:battery_plus_platform_interface/battery_plus_platform_interface.dart';
-import 'package:battery_plus_platform_interface/src/method_channel_battery_plus.dart';
+import 'package:battery_plus_platform_interface/method_channel_battery_plus.dart';
 import 'package:battery_plus_linux/battery_plus_linux.dart';
 
 // Export enums from the platform_interface so plugin users can use them directly.
--- packages/battery_plus_linux/CHANGELOG.md
+++ packages/battery_plus_linux/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.1
+
+- Address pub score
+
 ## 0.2.0
 
 - Added "unknown" battery state for batteryless systems.
