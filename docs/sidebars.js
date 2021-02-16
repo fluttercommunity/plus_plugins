6f4801989de0a42bc642b74730424da7b194f688
pr_Mais <hiimechi@gmail.com>
2021-02-17 00:29:26 +0300

    initial connectivity docs files

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,7 +2,7 @@ function toReferenceAPI(plugin) {
   return {
     type: "link",
     label: "Reference API",
-    href: `https://pub.dev/documentation/${plugin}/latest/`,
+    href: `https://pub.dev/documentation/${plugin}_plus/latest/`,
   };
 }
 
@@ -10,5 +10,6 @@ module.exports = {
   main: {
     "Getting Started": ["overview", "migration_guide"],
     Battery: ["battery/overview","battery/usage", toReferenceAPI("battery")],
+    Connectivity: ["connectivity/overview", "connectivity/usage", toReferenceAPI("connectivity")]
   },
 };
