d38424d246e3a1eab67206052217caa396e2ec42
Matej Zidek <zidek.matt@gmail.com>
2021-02-19 23:47:51 +0100

    [connectivity_plus_example] revert some potentially unwanted changes what were made during migration

--- packages/connectivity_plus/example/windows/flutter/generated_plugin_registrant.cc
+++ packages/connectivity_plus/example/windows/flutter/generated_plugin_registrant.cc
@@ -4,9 +4,4 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
-
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
-}
+void RegisterPlugins(flutter::PluginRegistry* registry) {}
--- packages/connectivity_plus/example/windows/flutter/generated_plugins.cmake
+++ packages/connectivity_plus/example/windows/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  connectivity_plus_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
