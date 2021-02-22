51241c87bd88bde031ee49ee17b64c11e7dba3c9
Miguel Beltran <m@beltran.work>
2021-02-22 09:03:16 +0100

    cleanup test

--- packages/battery_plus/test/battery_test.dart
+++ packages/battery_plus/test/battery_test.dart
@@ -7,7 +7,6 @@ import 'dart:async';
 import 'package:async/async.dart';
 import 'package:battery_plus/battery_plus.dart';
 import 'package:battery_plus_platform_interface/battery_plus_platform_interface.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:test/test.dart';
 
 late StreamController<BatteryState> controller;
