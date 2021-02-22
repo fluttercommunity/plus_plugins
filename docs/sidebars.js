8f5682a0868b8bbf233d265d8e4a7749de4ab388
Mais <41123719+pr-Mais@users.noreply.github.com>
2021-02-22 22:02:25 +0300

    [connectivity_plus] website docs + example app (#161)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,13 +2,14 @@ function toReferenceAPI(plugin) {
   return {
     type: "link",
     label: "Reference API",
-    href: `https://pub.dev/documentation/${plugin}/latest/`,
+    href: `https://pub.dev/documentation/${plugin}_plus/latest/`,
   };
 }
 
 module.exports = {
   main: {
     "Getting Started": ["overview", "migration_guide"],
-    Battery: ["battery/overview","battery/usage", toReferenceAPI("battery")],
+    Battery: ["battery/overview", "battery/usage", toReferenceAPI("battery")],
+    Connectivity: ["connectivity/overview", "connectivity/usage", toReferenceAPI("connectivity")]
   },
 };
