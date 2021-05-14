08a698cd9b1adb63e36202b736df0960464c2473
Majid <mhadaily@gmail.com>
2021-05-14 19:34:04 +0200

    cleanup

--- packages/package_info_plus_web/test/package_info_plus_web_test.dart
+++ packages/package_info_plus_web/test/package_info_plus_web_test.dart
@@ -28,33 +28,13 @@ void main() {
   test('Tell the user where to find the real tests', () {
     print('---');
     print('This package uses integration_test for its tests.');
-    print('See `example/README.md` for more info.');
+    print('See `interation_test` for more info.');
     print('---');
   });
 
   group(
     'Package Info Web',
     () {
-      test(
-        'Get correct valaues when response status is 200',
-        () async {
-          when(client.get(any)).thenAnswer(
-            (_) => Future.value(
-              http.Response(jsonEncode(VERSION_JSON), 200),
-            ),
-          );
-          final versionMap = await plugin.getAll();
-          print(versionMap.appName);
-          print(versionMap.version);
-          print(versionMap.packageName);
-          print(versionMap.buildNumber);
-          // expect(versionMap.appName, VERSION_JSON['appName']);
-          // expect(versionMap.buildNumber, VERSION_JSON['buildNumber']);
-          // expect(versionMap.packageName, '');
-          // expect(versionMap.version, VERSION_JSON['version']);
-        },
-      );
-
       test(
         'Get empty valaues when response status is not 200',
         () async {
@@ -66,10 +46,10 @@ void main() {
 
           final versionMap = await plugin.getAll();
 
-          expect(versionMap.appName, '');
-          expect(versionMap.buildNumber, '');
-          expect(versionMap.packageName, '');
-          expect(versionMap.version, '');
+          expect(versionMap.appName, isEmpty);
+          expect(versionMap.buildNumber, isEmpty);
+          expect(versionMap.packageName, isEmpty);
+          expect(versionMap.version, isEmpty);
         },
       );
     },
