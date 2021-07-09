f3d4a4e893d3ef2d65f12f10df6cd8709eb5d98a
Dimitri Dessus <d.dessus@apparence.io>
2021-07-09 09:43:54 +0200

    ✨ Add iOS submask + gateway + broadcast + ipv6

---
+++ packages/network_info_plus/network_info_plus/ios/Classes/getgateway.h
@@ -0,0 +1,13 @@
+//
+//  getgateway.h
+//  network_info_plus
+//
+//  Created by Dimitri Dessus on 08/07/2021.
+//
+
+#ifndef getgateway_h
+#define getgateway_h
+
+int getDefaultGateway(in_addr_t * addr);
+
+#endif /* getgateway_h */
