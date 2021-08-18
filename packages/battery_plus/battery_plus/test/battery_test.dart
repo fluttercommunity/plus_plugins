5c9df7b8de599819a9d4c9275a8ce2b74e85f546
Christian Padilla <christianepadilla@gmail.com>
2021-08-18 11:53:00 -0700

    Fix test formatting

--- packages/battery_plus/battery_plus/test/battery_test.dart
+++ packages/battery_plus/battery_plus/test/battery_test.dart
@@ -63,7 +63,7 @@ void main() {
 
       controller.add(BatteryState.charging);
       expect(await queue.next, BatteryState.charging);
-      
+
       controller.add(BatteryState.unknown);
       expect(await queue.next, BatteryState.unknown);
     });
