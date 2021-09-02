8b769980014f49586b33223cc126c2113695fb6f
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-09-02 15:39:48 +0200

    test: fix integration test

--- packages/package_info_plus/package_info_plus/example/test_driver/package_info_plus_e2e.dart
+++ packages/package_info_plus/package_info_plus/example/test_driver/package_info_plus_e2e.dart
@@ -19,9 +19,9 @@ void main() {
     // These tests are based on the example app. The tests should be updated if any related info changes.
     if (Platform.isAndroid) {
       expect(info.appName, 'package_info_example');
-      expect(info.buildNumber, '1');
+      expect(info.buildNumber, '4');
       expect(info.packageName, 'io.flutter.plugins.packageinfoexample');
-      expect(info.version, '1.0');
+      expect(info.version, '1.2.3');
     } else if (Platform.isIOS) {
       expect(info.appName, 'Package Info Example');
       expect(info.buildNumber, '1');
@@ -52,10 +52,10 @@ void main() {
     await tester.pumpAndSettle();
     if (Platform.isAndroid) {
       expect(find.text('package_info_example'), findsOneWidget);
-      expect(find.text('1'), findsOneWidget);
+      expect(find.text('4'), findsOneWidget);
       expect(
           find.text('io.flutter.plugins.packageinfoexample'), findsOneWidget);
-      expect(find.text('1.0'), findsOneWidget);
+      expect(find.text('1.2.3'), findsOneWidget);
     } else if (Platform.isIOS) {
       expect(find.text('Package Info Example'), findsOneWidget);
       expect(find.text('1'), findsOneWidget);
