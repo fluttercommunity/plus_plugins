c371e9aa2efa9f3e3e1a86951156559cc59b357e
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-10-01 22:59:09 +0200

    bug: added registry import

--- packages/package_info_plus/package_info_plus/android/src/main/java/dev/fluttercommunity/plus/packageinfo/PackageInfoPlugin.java
+++ packages/package_info_plus/package_info_plus/android/src/main/java/dev/fluttercommunity/plus/packageinfo/PackageInfoPlugin.java
@@ -18,6 +18,7 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
