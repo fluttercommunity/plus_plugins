03d8ec8275680f0baf71bc0c7829006e31be4744
Majid <mhadaily@gmail.com>
2021-10-03 22:23:00 +0200

    fix comments

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -2,6 +2,7 @@
 
 flutter config --no-analytics
 flutter pub global activate melos
+dart pub global run flutter_plugin_tools format
 echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
