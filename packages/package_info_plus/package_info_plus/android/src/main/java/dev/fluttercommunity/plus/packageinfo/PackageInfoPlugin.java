f1ea833c04b00f5bda384186b7fea08e0c09b1c2
Julian Steenbakker <juliansteenbakker@outlook.com>
2021-10-04 11:52:23 +0200

    imp: removed unused import

--- packages/package_info_plus/package_info_plus/android/src/main/java/dev/fluttercommunity/plus/packageinfo/PackageInfoPlugin.java
+++ packages/package_info_plus/package_info_plus/android/src/main/java/dev/fluttercommunity/plus/packageinfo/PackageInfoPlugin.java
@@ -13,12 +13,10 @@ import android.os.Build;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
