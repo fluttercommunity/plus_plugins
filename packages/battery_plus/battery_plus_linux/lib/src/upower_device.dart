c0cf9086f7c0682f882e35aeb74b9d1042960f2c
Volodymyr Buberenko <v.buberenko@gmail.com>
2021-10-13 01:15:22 +0300

    Change line numbers to not influence coverage

--- packages/battery_plus/battery_plus_linux/lib/src/upower_device.dart
+++ packages/battery_plus/battery_plus_linux/lib/src/upower_device.dart
@@ -44,16 +44,14 @@ class UPowerDevice extends DBusRemoteObject {
   Future<double> getPercentage() {
     return getProperty(_kDeviceAddress, 'Percentage').then(
       (value) => (value as DBusDouble).value,
-      onError: (error) =>
-          developer.log("Couldn't get percentage", error: error),
+      onError: (e) => developer.log("Couldn't get percentage", error: e),
     );
   }
 
   Future<UPowerBatteryState> getState() {
     return getProperty(_kDeviceAddress, 'State').then(
       (value) => (value as DBusUint32).value.toBatteryState(),
-      onError: (error) =>
-          developer.log("Couldn't get battery state", error: error),
+      onError: (e) => developer.log("Couldn't get battery state", error: e),
     );
   }
 
