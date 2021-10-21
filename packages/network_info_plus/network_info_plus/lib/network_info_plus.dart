226809291117e8abf7c527451cf51ca1bf729435
pr_Mais <hiimechi@gmail.com>
2021-10-21 21:44:18 +0300

    (network_info): remove unused imports

--- packages/network_info_plus/network_info_plus/lib/network_info_plus.dart
+++ packages/network_info_plus/network_info_plus/lib/network_info_plus.dart
@@ -3,13 +3,9 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:io' show Platform;
 
 import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:network_info_plus_platform_interface/network_info_plus_platform_interface.dart';
-import 'package:network_info_plus_platform_interface/method_channel_network_info.dart';
-import 'package:network_info_plus_linux/network_info_plus_linux.dart';
 
 // Export enums from the platform_interface so plugin users can use them directly.
 export 'package:network_info_plus_platform_interface/network_info_plus_platform_interface.dart'
