0a6c89c520c10db6a7603a74c8f6ac3bf988ddbc
pr_Mais <hiimechi@gmail.com>
2021-10-22 13:58:04 +0300

    test(battery_plus): test platfrom registration

--- packages/battery_plus/battery_plus_linux/lib/src/battery_plus_linux_real.dart
+++ packages/battery_plus/battery_plus_linux/lib/src/battery_plus_linux_real.dart
@@ -27,11 +27,9 @@ typedef UPowerDeviceFactory = UPowerDevice Function();
 /// The Linux implementation of BatteryPlatform.
 class BatteryPlusLinux extends BatteryPlatform {
   /// Register this dart class as the platform implementation for linux
-  // coverage:ignore-start: starts ignoring all next lines
   static void registerWith() {
     BatteryPlatform.instance = BatteryPlusLinux();
   }
-  // coverage:ignore-end: stops ignoring lines
 
   /// Returns the current battery level in percent.
   @override
