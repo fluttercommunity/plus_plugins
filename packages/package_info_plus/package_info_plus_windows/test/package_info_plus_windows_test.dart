4fdca6bf70492d7c1af88dabc30e79bb4253eb52
J-P Nurmi <jpnurmi@gmail.com>
2021-11-03 11:25:40 +0100

    [package_info_plus_windows] get rid of the _real vs. _stub hack (#599)

--- packages/package_info_plus/package_info_plus_windows/test/package_info_plus_windows_test.dart
+++ packages/package_info_plus/package_info_plus_windows/test/package_info_plus_windows_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:package_info_plus_platform_interface/package_info_platform_interface.dart';
-import 'package:package_info_plus_windows/src/package_info_plus_windows_real.dart';
+import 'package:package_info_plus_windows/src/package_info_plus_windows.dart';
 
 void main() {
   test('registered instance', () {
