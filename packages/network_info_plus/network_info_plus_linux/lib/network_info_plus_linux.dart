765b19b1fdb4312d4af5125007e928fd447f815a
J-P Nurmi <jpnurmi@gmail.com>
2021-11-03 11:25:20 +0100

    [network_info_plus_linux] get rid of the _real vs. _stub hack (#597)
    This hack is no longer required as dbus.dart has a conditional dart:ffi
    import (since 0.3.2) to avoid breaking web as an indirect dependency.

--- packages/network_info_plus/network_info_plus_linux/lib/network_info_plus_linux.dart
+++ packages/network_info_plus/network_info_plus_linux/lib/network_info_plus_linux.dart
@@ -1,8 +1,4 @@
 /// The Linux implementation of `network_info_plus`.
 library network_info_plus_linux;
 
-// network_info_plus_linux depends on dbus which uses FFI internally; export
-// a stub for platforms that don't support FFI (e.g., web) to avoid having
-// transitive dependencies break web compilation.
-export 'src/network_info_stub.dart'
-    if (dart.library.ffi) 'src/network_info_real.dart';
+export 'src/network_info.dart';
