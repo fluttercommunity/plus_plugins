5b1fb8349c3dc748b76b8fdfa21e05211e3e1a1a
J-P Nurmi <jpnurmi@gmail.com>
2021-11-03 11:24:57 +0100

    [battery_plus_linux] get rid of the _real vs. _stub hack (#595)
    This hack is no longer required as dbus.dart has a conditional dart:ffi
    import (since 0.3.2) to avoid breaking web as an indirect dependency.

--- packages/battery_plus/battery_plus_linux/lib/battery_plus_linux.dart
+++ packages/battery_plus/battery_plus_linux/lib/battery_plus_linux.dart
@@ -1,8 +1,4 @@
 /// The Linux implementation of `battery_plus`.
 library battery_plus_linux;
 
-// battery_plus_linux depends on dbus which uses FFI internally; export a stub
-// for platforms that don't support FFI (e.g., web) to avoid having transitive
-// dependencies break web compilation.
-export 'src/battery_plus_linux_stub.dart'
-    if (dart.library.ffi) 'src/battery_plus_linux_real.dart';
+export 'src/battery_plus_linux.dart';
