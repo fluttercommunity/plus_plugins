cf2f85155fdc3e662e9bdd8f30e220e60711e38d
J-P Nurmi <jpnurmi@gmail.com>
2021-11-03 10:52:19 +0100

    [package_info_plus_windows] get rid of the _real vs. _stub hack

--- packages/package_info_plus/package_info_plus_windows/lib/package_info_plus_windows.dart
+++ packages/package_info_plus/package_info_plus_windows/lib/package_info_plus_windows.dart
@@ -1,5 +1,1 @@
-// package_info_plus_windows is implemented using FFI; export a stub for
-// platforms that don't support FFI (e.g., web) to avoid having transitive
-// dependencies break web compilation.
-export 'src/package_info_plus_windows_stub.dart'
-    if (dart.library.ffi) 'src/package_info_plus_windows_real.dart';
+export 'src/package_info_plus_windows.dart';
