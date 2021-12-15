fa1934b8171bb2b81342963cda7f3b6b5c0476e1
Miguel Beltran <m@beltran.work>
2021-12-15 13:40:03 +0100

    Fix lint issues from Flutter 2.8.0

--- packages/android_alarm_manager_plus/example/test_driver/android_alarm_manager_plus_e2e.dart
+++ packages/android_alarm_manager_plus/example/test_driver/android_alarm_manager_plus_e2e.dart
@@ -2,7 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
 import 'dart:io';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:e2e/e2e.dart';
