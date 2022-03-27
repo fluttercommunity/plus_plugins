13ecad94695b96e57d2a8d2f01771229471a1f7c
martinoyovo <martinosoyen@gmail.com>
2022-03-27 02:47:18 +0000

    Update MethodCallHandlerImpl.java

--- packages/device_info_plus/device_info_plus/android/src/main/java/dev/fluttercommunity/plus/device_info/MethodCallHandlerImpl.java
+++ packages/device_info_plus/device_info_plus/android/src/main/java/dev/fluttercommunity/plus/device_info/MethodCallHandlerImpl.java
@@ -98,8 +98,8 @@ class MethodCallHandlerImpl implements MethodChannel.MethodCallHandler {
   }
 
   /**
-   * Returns the Android hardware device ID that is unique between the device + user and app
-   * signing. This key will change if a factory reset is performed on the device or if an APK signing key changes.
+   * Returns the Android hardware device ID that is unique between the device + user and app signing.
+   * This key will change if a factory reset is performed on the device or if an APK signing key changes.
    * @return The android ID
    */
   @SuppressLint("HardwareIds")
