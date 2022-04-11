3ca12f2d257d7425b43d79f698ee3c8df8b80d0c
İlhan Aydınlı <13945364+ilhanaydinli@users.noreply.github.com>
2022-04-11 10:26:10 +0300

    fix(connectivity_plus): Reachability.swift ".unavailable" is deprecated (#823)
    * [connectivity_plus] .unavailable deprecated
    
    * [connectivity_plus] Update changelog and version

--- packages/connectivity_plus/connectivity_plus/ios/Classes/ReachabilityConnectivityProvider.swift
+++ packages/connectivity_plus/connectivity_plus/ios/Classes/ReachabilityConnectivityProvider.swift
@@ -6,7 +6,7 @@ public class ReachabilityConnectivityProvider: NSObject, ConnectivityProvider {
 
   public var currentConnectivityType: ConnectivityType {
     let reachability = try? self.reachability ?? Reachability()
-    switch reachability?.connection ?? .unavailable {
+    switch reachability?.connection {
     case .wifi:
       return .wifi
     case .cellular:
