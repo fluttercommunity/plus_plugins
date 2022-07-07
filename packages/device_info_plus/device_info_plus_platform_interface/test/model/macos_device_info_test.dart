f92c14b510f15b1c93517fc74eef706b60c4717a
iapicca <iapicca@gmail.com>
2022-07-07 19:46:03 +0300

    [device_info_plus_platform_interface] remove duplicated tests

--- packages/device_info_plus/device_info_plus_platform_interface/test/model/macos_device_info_test.dart
+++ packages/device_info_plus/device_info_plus_platform_interface/test/model/macos_device_info_test.dart
@@ -16,40 +16,6 @@ void main() {
         'osRelease': 'osRelease',
         'computerName': 'computerName',
         'kernelVersion': 'kernelVersion',
-        'systemGUID': null,
-      };
-
-      test('fromMap should return $MacOsDeviceInfo with correct values', () {
-        final macosDeviceInfo = MacOsDeviceInfo.fromMap(macosDeviceInfoMap);
-
-        expect(macosDeviceInfo.arch, 'arch');
-        expect(macosDeviceInfo.model, 'model');
-        expect(macosDeviceInfo.activeCPUs, 4);
-        expect(macosDeviceInfo.memorySize, 16);
-        expect(macosDeviceInfo.cpuFrequency, 2);
-        expect(macosDeviceInfo.hostName, 'hostName');
-        expect(macosDeviceInfo.osRelease, 'osRelease');
-        expect(macosDeviceInfo.systemGUID, isNull);
-      });
-
-      test('toMap should return map with correct key and map', () {
-        final macosDeviceInfo = MacOsDeviceInfo.fromMap(macosDeviceInfoMap);
-
-        expect(macosDeviceInfo, macosDeviceInfoMap);
-      });
-    });
-
-    group('fromMap | toMap', () {
-      const macosDeviceInfoMap = <String, dynamic>{
-        'arch': 'arch',
-        'model': 'model',
-        'activeCPUs': 4,
-        'memorySize': 16,
-        'cpuFrequency': 2,
-        'hostName': 'hostName',
-        'osRelease': 'osRelease',
-        'computerName': 'computerName',
-        'kernelVersion': 'kernelVersion',
         'systemGUID': 'systemGUID',
       };
 
@@ -74,7 +40,7 @@ void main() {
       test('jsonEncode / jsonDecode should return the correct map', () {
         final macosDeviceInfo = MacOsDeviceInfo.fromMap(macosDeviceInfoMap);
 
-        final json = jsonEncode(macosDeviceInfo.toJson());
+        final json = jsonEncode(macosDeviceInfo);
 
         expect(jsonDecode(json), macosDeviceInfoMap);
       });
