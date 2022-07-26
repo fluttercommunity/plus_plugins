466de6fe27c67986ba8d21cf131e9ae36a62c275
CheverJohn <cheverjonathan@gmail.com>
2022-07-26 09:44:09 +0800

    style(connectivity_plus): flutter format

--- packages/connectivity_plus/connectivity_plus/example/integration_test/connectivity_plus_test.dart
+++ packages/connectivity_plus/connectivity_plus/example/integration_test/connectivity_plus_test.dart
@@ -4,7 +4,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
 import 'dart:io';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
