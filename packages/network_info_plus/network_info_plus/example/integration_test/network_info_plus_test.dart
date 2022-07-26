299a6f414a2198caa95221ba7e2c37ed1eef528a
CheverJohn <cheverjonathan@gmail.com>
2022-07-26 14:05:38 +0800

    test(network_info_plus): fix the code review

--- packages/network_info_plus/network_info_plus/example/integration_test/network_info_plus_test.dart
+++ packages/network_info_plus/network_info_plus/example/integration_test/network_info_plus_test.dart
@@ -5,51 +5,18 @@
 // @dart=2.9
 
 import 'dart:io';
-import 'package:flutter/services.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:network_info_plus/network_info_plus.dart';
-import 'package:network_info_plus_platform_interface/method_channel_network_info.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('NetworkInfo test driver', () {
-    final log = <MethodCall>[];
     NetworkInfo _networkInfo;
-    MethodChannelNetworkInfo _methodChannelNetworkInfo;
 
     setUpAll(() async {
       _networkInfo = NetworkInfo();
-      _methodChannelNetworkInfo = MethodChannelNetworkInfo();
-
-      _methodChannelNetworkInfo.methodChannel
-          .setMockMethodCallHandler((MethodCall methodCall) async {
-        log.add(methodCall);
-        switch (methodCall.method) {
-          case 'wifiName':
-            return '1337wifi';
-          case 'wifiBSSID':
-            return 'c0:ff:33:c0:d3:55';
-          case 'wifiIPAddress':
-            return '127.0.0.1';
-          case 'wifiIPv6Address':
-            return '2002:7f00:0001:0:0:0:0:0';
-          case 'wifiBroadcast':
-            return '127.0.0.255';
-          case 'wifiGatewayAddress':
-            return '127.0.0.0';
-          case 'wifiSubmask':
-            return '255.255.255.0';
-          case 'requestLocationServiceAuthorization':
-            return 'authorizedAlways';
-          case 'getLocationServiceAuthorization':
-            return 'authorizedAlways';
-          default:
-            return null;
-        }
-      });
-      log.clear();
     });
 
     testWidgets('test location methods, iOS only', (WidgetTester tester) async {
@@ -57,7 +24,7 @@ void main() {
         expect((await _networkInfo.getLocationServiceAuthorization()),
             LocationAuthorizationStatus.notDetermined);
       }
-    }, skip: !Platform.isAndroid);
+    }, skip: !Platform.isIOS);
 
     testWidgets('test non-null network value', (WidgetTester tester) async {
       expect(_networkInfo.getWifiName(), isNotNull);
@@ -67,20 +34,6 @@ void main() {
       expect(_networkInfo.getWifiSubmask(), isNotNull);
       expect(_networkInfo.getWifiGatewayIP(), isNotNull);
       expect(_networkInfo.getWifiBroadcast(), isNotNull);
-    }, skip: !Platform.isAndroid);
-
-    testWidgets('test fixed network value', (WidgetTester tester) async {
-      final result = await _methodChannelNetworkInfo.getWifiSubmask();
-      expect(result, '255.255.255.0');
-      expect(
-        log,
-        <Matcher>[
-          isMethodCall(
-            'wifiSubmask',
-            arguments: null,
-          ),
-        ],
-      );
-    }, skip: !Platform.isAndroid);
+    });
   });
 }
