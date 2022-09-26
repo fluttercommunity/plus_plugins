e858786bc3b6e0c07d1f05fbc6d615051766b7b4
Miguel Beltran <m@beltran.work>
2022-09-26 14:15:29 +0200

    linux setup WIP

--- .github/workflows/scripts/build-examples.sh
+++ .github/workflows/scripts/build-examples.sh
@@ -33,7 +33,7 @@ fi
 
 if [ "$ACTION" == "linux" ]
 then
-  sudo apt-get install ninja-build
+  sudo apt-get install ninja-build libgtk-3-dev
   flutter doctor -v
   melos exec -c 1 --scope="$PLUGIN_EXAMPLE_SCOPE" \
     -- flutter build linux $FLUTTER_COMMAND_FLAGS --debug --target="$TARGET_FILE" --dart-define=CI=true
