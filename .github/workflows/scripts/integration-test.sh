d2063adf2c9022af27280ae71b66a5610ba706ff
Miguel Beltran <m@beltran.work>
2022-09-26 17:04:56 +0200

    enable linux integration tests for share_plus_example

--- .github/workflows/scripts/integration-test.sh
+++ .github/workflows/scripts/integration-test.sh
@@ -3,11 +3,16 @@
 ACTION=$1
 SCOPE=$2
 
-# At the moment, only Android integration tests run
 if [ "$ACTION" == "android" ]
 then
   # Sleep to allow emulator to settle.
 #  sleep 15
+  melos exec -c 1 --scope="$SCOPE" --dir-exists="./integration_test" -- \
+    "flutter test ./integration_test/MELOS_PARENT_PACKAGE_NAME_test.dart --dart-define=CI=true"
+fi
+
+if [ "$ACTION" == "linux" ]
+then
   melos exec -c 1 --scope="$SCOPE" --dir-exists="./integration_test" -- \
     "flutter test ./integration_test/MELOS_PARENT_PACKAGE_NAME_test.dart --dart-define=CI=true"
 fi
