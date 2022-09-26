cf967643432dc98a78402a2762ed22f53a22aae5
Miguel Beltran <m@beltran.work>
2022-09-26 17:09:32 +0200

    WIP Linux fix

--- .github/workflows/scripts/integration-test.sh
+++ .github/workflows/scripts/integration-test.sh
@@ -5,14 +5,13 @@ SCOPE=$2
 
 if [ "$ACTION" == "android" ]
 then
-  # Sleep to allow emulator to settle.
-#  sleep 15
   melos exec -c 1 --scope="$SCOPE" --dir-exists="./integration_test" -- \
     "flutter test ./integration_test/MELOS_PARENT_PACKAGE_NAME_test.dart --dart-define=CI=true"
 fi
 
 if [ "$ACTION" == "linux" ]
 then
+  sudo apt-get install ninja-build libgtk-3-dev
   melos exec -c 1 --scope="$SCOPE" --dir-exists="./integration_test" -- \
-    "flutter test ./integration_test/MELOS_PARENT_PACKAGE_NAME_test.dart --dart-define=CI=true"
+    "flutter test -d linux ./integration_test/MELOS_PARENT_PACKAGE_NAME_test.dart --dart-define=CI=true"
 fi
