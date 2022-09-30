dec49055c1d41c4ce6facec3c489755b3b462303
Joshua Matthews <josh@jmatth.com>
2022-09-30 15:39:06 -0400

    Adding Windows support to integration test script

--- .github/workflows/scripts/integration-test.sh
+++ .github/workflows/scripts/integration-test.sh
@@ -16,6 +16,12 @@ then
     "flutter test -d linux ./integration_test/MELOS_PARENT_PACKAGE_NAME_test.dart --dart-define=CI=true"
 fi
 
+if [ "$ACTION" == "windows" ]
+then
+  melos exec -c 1 --scope="$SCOPE" --dir-exists="./integration_test" -- \
+    "flutter test -d windows ./integration_test/MELOS_PARENT_PACKAGE_NAME_test.dart --dart-define=CI=true"
+fi
+
 if [ "$ACTION" == "macos" ]
 then
   melos exec -c 1 --scope="$SCOPE" --dir-exists="./integration_test" -- \
