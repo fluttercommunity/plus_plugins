e942e437fdfcc046f361c11b6cf43bb9ffac751f
Joshua Matthews <josh@jmatth.com>
2022-09-30 22:38:21 -0400

    Removing flutter_plugin_tools command from install-tools script

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -4,7 +4,6 @@ set -e
 
 flutter config --no-analytics
 flutter pub global activate melos
-# dart pub global run flutter_plugin_tools format
 echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
 echo "$HOME/AppData/Local/Pub/Cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
