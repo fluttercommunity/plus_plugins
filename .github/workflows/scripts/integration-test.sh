dc18b00005ff2b2009e1c435635de73830ad11b0
TB <tiberiulepadatu14@gmail.com>
2022-10-02 18:25:05 +0300

    [linux test integration] - fix 1104
    * remove wrong merge issues

--- .github/workflows/scripts/integration-test.sh
+++ .github/workflows/scripts/integration-test.sh
@@ -13,9 +13,12 @@ fi
 
 if [ "$ACTION" == "linux" ]
 then
-  sudo apt-get install ninja-build libgtk-3-dev
+  sudo apt-get update
+  sudo apt-get install -y clang cmake ninja-build libgtk-3-dev xvfb
+  export DISPLAY=:99
+  sudo Xvfb -ac :99 -screen 0 1280x1024x24 > /dev/null 2>&1 &
   melos exec -c 1 --scope="$SCOPE" --dir-exists="./integration_test" -- \
-    "flutter test -d linux ./integration_test/MELOS_PARENT_PACKAGE_NAME_test.dart --dart-define=CI=true"
+  "flutter test -d linux ./integration_test/MELOS_PARENT_PACKAGE_NAME_test.dart --dart-define=CI=true"
 fi
 
 if [ "$ACTION" == "windows" ]
