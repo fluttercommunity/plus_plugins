6a114eb2e5566b8789e18b12718572daefd35bcb
“abd99” <abdullahzakir99@gmail.com>
2022-10-02 23:03:35 +0530

    Add share_plus tests for Linux

--- packages/share_plus/share_plus/example/integration_test/share_plus_test.dart
+++ packages/share_plus/share_plus/example/integration_test/share_plus_test.dart
@@ -7,8 +7,8 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +20,10 @@ void main() {
   testWidgets('Can launch share in MacOS', (WidgetTester tester) async {
     expect(Share.share('message', subject: 'title'), isNotNull);
   }, skip: !Platform.isMacOS);
+
+  testWidgets('Can launch share on Linux', (WidgetTester tester) async {
+    expect(Share.share('message', subject: 'title'), isNotNull);
+  }, skip: !Platform.isLinux);
 
   testWidgets('Can launch shareWithResult', (WidgetTester tester) async {
     expect(Share.shareWithResult('message', subject: 'title'), isNotNull);
