66ef2523cbb59348bab6cb2bb8163ef4efd5e1c8
Miguel Beltran <m@beltran.work>
2022-10-06 09:30:17 +0200

    rename BatteryPlusWebPlugin

--- packages/battery_plus/lib/src/battery_plus_web.dart
+++ packages/battery_plus/lib/src/battery_plus_web.dart
@@ -7,9 +7,9 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import '../battery_plus_platform_interface.dart';
 
 /// The web implementation of the BatteryPlatform of the Battery plugin.
-class BatteryPlusPlugin extends BatteryPlatform {
+class BatteryPlusWebPlugin extends BatteryPlatform {
   /// Constructs a BatteryPlusPlugin.
-  BatteryPlusPlugin(html.Navigator navigator)
+  BatteryPlusWebPlugin(html.Navigator navigator)
       : _getBattery = navigator.getBattery;
 
   /// A check to determine if this version of the plugin can be used.
@@ -21,7 +21,7 @@ class BatteryPlusPlugin extends BatteryPlatform {
   /// Factory method that initializes the Battery plugin platform with an instance
   /// of the plugin for the web.
   static void registerWith(Registrar registrar) {
-    BatteryPlatform.instance = BatteryPlusPlugin(html.window.navigator);
+    BatteryPlatform.instance = BatteryPlusWebPlugin(html.window.navigator);
   }
 
   /// Returns the current battery level in percent.
