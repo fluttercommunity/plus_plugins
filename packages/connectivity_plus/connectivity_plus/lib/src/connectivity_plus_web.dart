b206f0ae87972f55e1b3a81753c7512e6026c607
Miguel Beltran <m@beltran.work>
2022-10-13 13:56:03 +0200

    refactor(connectivity_plus)!: two-package federated architecture (#1227)

--- packages/connectivity_plus/connectivity_plus/lib/src/connectivity_plus_web.dart
+++ packages/connectivity_plus/connectivity_plus/lib/src/connectivity_plus_web.dart
@@ -1,11 +1,11 @@
 import 'package:connectivity_plus_platform_interface/connectivity_plus_platform_interface.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-import 'src/network_information_api_connectivity_plugin.dart';
-import 'src/dart_html_connectivity_plugin.dart';
+import 'web/network_information_api_connectivity_plugin.dart';
+import 'web/dart_html_connectivity_plugin.dart';
 
 /// The web implementation of the ConnectivityPlatform of the Connectivity plugin.
-class ConnectivityPlusPlugin extends ConnectivityPlatform {
+class ConnectivityPlusWebPlugin extends ConnectivityPlatform {
   /// Factory method that initializes the connectivity plugin platform with an instance
   /// of the plugin for the web.
   static void registerWith(Registrar registrar) {
--- packages/connectivity_plus/connectivity_plus_linux/lib/connectivity_plus_linux.dart
+++
@@ -1,4 +0,0 @@
-/// The Linux implementation of `connectivity_plus`.
-library connectivity_plus_linux;
-
-export 'src/connectivity.dart';
