ba31fe2eaec4867d98e8d000bef435a6a30aaad5
Volodymyr Buberenko <v.buberenko@gmail.com>
2022-10-16 19:13:21 +0300

    Introduce single fake model for Android tests

--- packages/device_info_plus/device_info_plus_platform_interface/test/model/android_device_info_test.dart
+++ packages/device_info_plus/device_info_plus_platform_interface/test/model/android_device_info_test.dart
@@ -3,57 +3,14 @@
 import 'package:device_info_plus_platform_interface/model/android_device_info.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'android_device_info_fake.dart';
+
 void main() {
   group('$AndroidDeviceInfo', () {
     group('fromMap | toMap', () {
-      const androidBuildVersionMap = <String, dynamic>{
-        'sdkInt': 16,
-        'baseOS': 'baseOS',
-        'previewSdkInt': 30,
-        'release': 'release',
-        'codename': 'codename',
-        'incremental': 'incremental',
-        'securityPatch': 'securityPatch',
-      };
-
-      const displayMetrics = <String, dynamic>{
-        'widthPx': 1080.0,
-        'heightPx': 2220.0,
-        'xDpi': 530.0859,
-        'yDpi': 529.4639,
-      };
-
-      const supportedAbis = <String>['arm64-v8a', 'x86', 'x86_64'];
-      const supported32BitAbis = <String?>['x86 (IA-32)', 'MMX'];
-      const supported64BitAbis = <String?>['x86-64', 'MMX', 'SSSE3'];
-      const systemFeatures = ['FEATURE_AUDIO_PRO', 'FEATURE_AUDIO_OUTPUT'];
-      final androidDeviceInfoMap = <String, dynamic>{
-        'id': 'id',
-        'host': 'host',
-        'tags': 'tags',
-        'type': 'type',
-        'model': 'model',
-        'board': 'board',
-        'brand': 'brand',
-        'device': 'device',
-        'product': 'product',
-        'display': 'display',
-        'hardware': 'hardware',
-        'isPhysicalDevice': true,
-        'bootloader': 'bootloader',
-        'fingerprint': 'fingerprint',
-        'manufacturer': 'manufacturer',
-        'supportedAbis': supportedAbis,
-        'systemFeatures': systemFeatures,
-        'version': androidBuildVersionMap,
-        'supported64BitAbis': supported64BitAbis,
-        'supported32BitAbis': supported32BitAbis,
-        'displayMetrics': displayMetrics,
-      };
-
       test('fromMap should return $AndroidDeviceInfo with correct values', () {
         final androidDeviceInfo =
-            AndroidDeviceInfo.fromMap(androidDeviceInfoMap);
+            AndroidDeviceInfo.fromMap(fakeAndroidDeviceInfo);
 
         expect(androidDeviceInfo.id, 'id');
         expect(androidDeviceInfo.host, 'host');
@@ -61,7 +18,7 @@ void main() {
         expect(androidDeviceInfo.type, 'type');
         expect(androidDeviceInfo.model, 'model');
         expect(androidDeviceInfo.board, 'board');
-        expect(androidDeviceInfo.brand, 'brand');
+        expect(androidDeviceInfo.brand, 'Google');
         expect(androidDeviceInfo.device, 'device');
         expect(androidDeviceInfo.product, 'product');
         expect(androidDeviceInfo.display, 'display');
@@ -70,10 +27,10 @@ void main() {
         expect(androidDeviceInfo.isPhysicalDevice, isTrue);
         expect(androidDeviceInfo.fingerprint, 'fingerprint');
         expect(androidDeviceInfo.manufacturer, 'manufacturer');
-        expect(androidDeviceInfo.supportedAbis, supportedAbis);
-        expect(androidDeviceInfo.systemFeatures, systemFeatures);
-        expect(androidDeviceInfo.supported32BitAbis, supported32BitAbis);
-        expect(androidDeviceInfo.supported64BitAbis, supported64BitAbis);
+        expect(androidDeviceInfo.supportedAbis, fakeSupportedAbis);
+        expect(androidDeviceInfo.systemFeatures, fakeSystemFeatures);
+        expect(androidDeviceInfo.supported32BitAbis, fakeSupported32BitAbis);
+        expect(androidDeviceInfo.supported64BitAbis, fakeSupported64BitAbis);
         expect(androidDeviceInfo.version.sdkInt, 16);
         expect(androidDeviceInfo.version.baseOS, 'baseOS');
         expect(androidDeviceInfo.version.previewSdkInt, 30);
@@ -89,9 +46,9 @@ void main() {
 
       test('toMap should return map with correct key and map', () {
         final androidDeviceInfo =
-            AndroidDeviceInfo.fromMap(androidDeviceInfoMap);
+            AndroidDeviceInfo.fromMap(fakeAndroidDeviceInfo);
 
-        expect(androidDeviceInfo.toMap(), androidDeviceInfoMap);
+        expect(androidDeviceInfo.toMap(), fakeAndroidDeviceInfo);
       });
     });
   });
