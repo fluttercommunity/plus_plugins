3bb18f225d38f4097678b8132369ad55bab13483
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2023-03-15 21:56:25 +0100

    ci(all): Migrate to Melos 3 (#1619)

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -3,7 +3,7 @@
 set -e
 
 flutter config --no-analytics
-flutter pub global activate melos 2.9.0
+flutter pub global activate melos 3.0.0
 echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
 echo "$HOME/AppData/Local/Pub/Cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
