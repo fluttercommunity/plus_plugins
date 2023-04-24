f14d890422ff9714703c22d99782c4b5ed42960a
Gabriel Terwesten <gabriel@terwesten.net>
2023-04-24 17:00:13 +0200

    run melos bootstrap in integration-test.sh

--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -8,8 +8,3 @@ echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
 echo "$HOME/AppData/Local/Pub/Cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
-
-$HOME/.pub-cache/bin/melos bs --scope "$PLUGIN_SCOPE"
-if [ "$PLUGIN_EXAMPLE_SCOPE" != "" ]; then
-  $HOME/.pub-cache/bin/melos bs --scope "$PLUGIN_EXAMPLE_SCOPE"
-fi
