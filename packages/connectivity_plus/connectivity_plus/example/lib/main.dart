f7448adc1d92c0e6621d8c781cab2e5cb5fb8fad
Volodymyr Buberenko <v.buberenko@gmail.com>
2023-05-01 10:30:18 +0300

    chore(connectivity_plus): Update Flutter dependencies, set Flutter >=3.3.0 and Dart to >=2.18.0 <4.0.0

--- packages/connectivity_plus/connectivity_plus/example/lib/main.dart
+++ packages/connectivity_plus/connectivity_plus/example/lib/main.dart
@@ -24,7 +24,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0x9f4376f8),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -92,6 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Connectivity example app'),
+        elevation: 4,
       ),
       body: Center(
           child: Text('Connection Status: ${_connectionStatus.toString()}')),
