4d5b60a615d12affd43e42374354bf94e268ae13
Tim Sneath <timsneath@google.com>
2023-05-02 11:05:37 -0700

    Apply platform test constraints

--- packages/package_info_plus/package_info_plus/test/package_info_plus_linux_test.dart
+++ packages/package_info_plus/package_info_plus/test/package_info_plus_linux_test.dart
@@ -1,3 +1,6 @@
+@TestOn('linux')
+library package_info_plus_linux_test;
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:package_info_plus_platform_interface/package_info_platform_interface.dart';
--- packages/package_info_plus/package_info_plus/test/package_info_plus_windows_test.dart
+++ packages/package_info_plus/package_info_plus/test/package_info_plus_windows_test.dart
@@ -1,4 +1,8 @@
+@TestOn('windows')
+library package_info_plus_windows_test;
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:package_info_plus/src/file_version_info.dart';
 import 'package:package_info_plus/src/package_info_plus_windows.dart';
 import 'package:package_info_plus_platform_interface/package_info_platform_interface.dart';
 
@@ -6,5 +10,13 @@ void main() {
   test('registered instance', () {
     PackageInfoPlusWindowsPlugin.registerWith();
     expect(PackageInfoPlatform.instance, isA<PackageInfoPlusWindowsPlugin>());
+  });
+
+  test('File version info works for standard Windows DLL', () {
+    // Every valid Windows release should have this file
+    final kernelVersion = FileVersionInfo('%WINDIR%\\System32\\kernel32.dll');
+    expect(kernelVersion.productName?.indexOf('Windows'), greaterThan(-1));
+    expect(kernelVersion.productVersion, isNotNull);
+    kernelVersion.dispose();
   });
 }
