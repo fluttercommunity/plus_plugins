c93cd345103e0c01e55d3fb2e15562bc8b03e224
Tim Sneath <timsneath@google.com>
2023-05-02 12:12:16 -0700

    Make FileVersionInfo non-nullable

--- packages/package_info_plus/package_info_plus/lib/src/file_version_info.dart
+++ packages/package_info_plus/package_info_plus/lib/src/file_version_info.dart
@@ -22,7 +22,7 @@ class LANGANDCODEPAGE extends Struct {
 
 class FileVersionInfoData {
   const FileVersionInfoData({required this.lpBlock, required this.lpLang});
-  final Pointer<Uint8> lpBlock;
+  final Pointer<BYTE> lpBlock;
   final Pointer<LANGANDCODEPAGE> lpLang;
 }
 
@@ -34,18 +34,18 @@ class FileVersionInfo {
 
   void dispose() => free(_data.lpBlock);
 
-  String? get companyName => getValue('CompanyName');
-  String? get companyShortName => getValue('CompanyShortName');
-  String? get productName => getValue('ProductName');
-  String? get productShortName => getValue('ProductShortName');
-  String? get internalName => getValue('InternalName');
-  String? get productVersion => getValue('ProductVersion');
-  String? get specialBuild => getValue('SpecialBuild');
-  String? get originalFilename => getValue('OriginalFilename');
-  String? get fileDescription => getValue('FileDescription');
-  String? get fileVersion => getValue('FileVersion');
+  String get companyName => getValue('CompanyName');
+  String get companyShortName => getValue('CompanyShortName');
+  String get productName => getValue('ProductName');
+  String get productShortName => getValue('ProductShortName');
+  String get internalName => getValue('InternalName');
+  String get productVersion => getValue('ProductVersion');
+  String get specialBuild => getValue('SpecialBuild');
+  String get originalFilename => getValue('OriginalFilename');
+  String get fileDescription => getValue('FileDescription');
+  String get fileVersion => getValue('FileVersion');
 
-  String? getValue(String name) {
+  String getValue(String name) {
     final langCodepages = [
       // try the language and codepage from the EXE
       [_data.lpLang.ref.wLanguage, _data.lpLang.ref.wCodePage],
@@ -57,30 +57,30 @@ class FileVersionInfo {
       [GetUserDefaultLangID(), 1252],
     ];
 
-    String? value;
     final Pointer<IntPtr> lplpBuffer = calloc<IntPtr>();
     final Pointer<Uint32> puLen = calloc<Uint32>();
-
     String toHex4(int val) => val.toRadixString(16).padLeft(4, '0');
 
-    for (final langCodepage in langCodepages) {
-      final lang = toHex4(langCodepage[0]);
-      final codepage = toHex4(langCodepage[1]);
-      final lpSubBlock = TEXT('\\StringFileInfo\\$lang$codepage\\$name');
-      final res =
-          VerQueryValue(_data.lpBlock, lpSubBlock, lplpBuffer.cast(), puLen);
-      free(lpSubBlock);
+    try {
+      for (final langCodepage in langCodepages) {
+        final lang = toHex4(langCodepage[0]);
+        final codepage = toHex4(langCodepage[1]);
+        final lpSubBlock = TEXT('\\StringFileInfo\\$lang$codepage\\$name');
+        final res =
+            VerQueryValue(_data.lpBlock, lpSubBlock, lplpBuffer.cast(), puLen);
+        free(lpSubBlock);
 
-      if (res != 0 && lplpBuffer.value != 0 && puLen.value > 0) {
-        final ptr = Pointer<Utf16>.fromAddress(lplpBuffer.value);
-        value = ptr.toDartString();
-        break;
+        if (res != 0 && lplpBuffer.value != 0 && puLen.value > 0) {
+          final ptr = Pointer<Utf16>.fromAddress(lplpBuffer.value);
+          return ptr.toDartString();
+        }
       }
-    }
 
-    calloc.free(lplpBuffer);
-    calloc.free(puLen);
-    return value;
+      return '';
+    } finally {
+      free(lplpBuffer);
+      free(puLen);
+    }
   }
 
   static FileVersionInfoData getData(String filePath) {
