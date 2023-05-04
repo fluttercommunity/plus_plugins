487f3462b1db2e108121a61ff12764089d3ce477
SamJakob <me@samjakob.com>
2023-05-04 11:14:01 +0100

    refactor(device_info_plus): Remove redundant checks for PRODUCT strings with sdk (#1745)

--- packages/device_info_plus/device_info_plus/android/src/main/kotlin/dev/fluttercommunity/plus/device_info/MethodCallHandlerImpl.kt
+++ packages/device_info_plus/device_info_plus/android/src/main/kotlin/dev/fluttercommunity/plus/device_info/MethodCallHandlerImpl.kt
@@ -107,7 +107,7 @@ internal class MethodCallHandlerImpl(
      * detection systems
      */
     private val isEmulator: Boolean
-        get() = (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic")
+        get() = ((Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
             || Build.FINGERPRINT.startsWith("generic")
             || Build.FINGERPRINT.startsWith("unknown")
             || Build.HARDWARE.contains("goldfish")
@@ -116,10 +116,7 @@ internal class MethodCallHandlerImpl(
             || Build.MODEL.contains("Emulator")
             || Build.MODEL.contains("Android SDK built for x86")
             || Build.MANUFACTURER.contains("Genymotion")
-            || Build.PRODUCT.contains("sdk_google")
-            || Build.PRODUCT.contains("google_sdk")
             || Build.PRODUCT.contains("sdk")
-            || Build.PRODUCT.contains("sdk_x86")
             || Build.PRODUCT.contains("vbox86p")
             || Build.PRODUCT.contains("emulator")
             || Build.PRODUCT.contains("simulator"))
