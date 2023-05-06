225830e76f103f19bce09c56cf1e9a4abea0d5f1
Volodymyr Buberenko <vbuberen@users.noreply.github.com>
2023-05-06 19:17:34 +0300

    chore(website): Remove null-safety page (#1788)

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -7,52 +7,79 @@ function toReferenceAPI(plugin) {
 }
 
 module.exports = {
-  main: {
-    "Getting Started": ["overview", "null_safety"],
-    ["Android Alarm Manager"]: [
-      "android_alarm_manager_plus/overview",
-      "android_alarm_manager_plus/usage",
-      toReferenceAPI("android_alarm_manager"),
+    main: [
+        'overview',
+        {
+            type: 'category',
+            label: 'Android Alarm Manager Plus',
+            items: [
+                "android_alarm_manager_plus/overview",
+                "android_alarm_manager_plus/usage",
+                toReferenceAPI("android_alarm_manager"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Battery Plus',
+            items: [
+                "battery_plus/overview",
+                "battery_plus/usage",
+                toReferenceAPI("battery"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Connectivity Plus',
+            items: [
+                "connectivity_plus/overview",
+                "connectivity_plus/usage",
+                toReferenceAPI("connectivity"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Device Info Plus',
+            items: [
+                "device_info_plus/overview",
+                "device_info_plus/usage",
+                toReferenceAPI("device_info"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Network Plus',
+            items: [
+                "network_info_plus/overview",
+                "network_info_plus/usage",
+                toReferenceAPI("network_info"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Package Info Plus',
+            items: [
+                "package_info_plus/overview",
+                "package_info_plus/usage",
+                toReferenceAPI("package_info"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Sensors Plus',
+            items: [
+                "sensors_plus/overview",
+                "sensors_plus/usage",
+                toReferenceAPI("sensors"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Share Plus',
+            items: [
+                "share_plus/overview",
+                "share_plus/usage",
+                toReferenceAPI("share"),
+            ],
+        },
     ],
-    ["Android Intent"]: [
-      "android_intent_plus/overview",
-      "android_intent_plus/usage",
-      toReferenceAPI("android_intent"),
-    ],
-    Battery: [
-      "battery_plus/overview",
-      "battery_plus/usage",
-      toReferenceAPI("battery"),
-    ],
-    Connectivity: [
-      "connectivity_plus/overview",
-      "connectivity_plus/usage",
-      toReferenceAPI("connectivity"),
-    ],
-    ["Device Info"]: [
-      "device_info_plus/overview",
-      "device_info_plus/usage",
-      toReferenceAPI("device_info"),
-    ],
-    ["Network Info"]: [
-      "network_info_plus/overview",
-      "network_info_plus/usage",
-      toReferenceAPI("network_info"),
-    ],
-    ["Package Info"]: [
-      "package_info_plus/overview",
-      "package_info_plus/usage",
-      toReferenceAPI("package_info"),
-    ],
-    ["Share"]: [
-      "share_plus/overview",
-      "share_plus/usage",
-      toReferenceAPI("share"),
-    ],
-    ["Sensors"]: [
-      "sensors_plus/overview",
-      "sensors_plus/usage",
-      toReferenceAPI("sensors"),
-    ],
-  },
 };
