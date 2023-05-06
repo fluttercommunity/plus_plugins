3d8b7d1913c1b0d35cbdd4dd0fb4dece3b7dc6c3
Volodymyr Buberenko <v.buberenko@gmail.com>
2023-05-06 16:51:58 +0300

    Update sidebar

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -7,52 +7,79 @@ function toReferenceAPI(plugin) {
 }
 
 module.exports = {
-  main: {
-    "Getting Started": ["overview"],
-    ["Android Alarm Manager"]: [
-      "android_alarm_manager_plus/overview",
-      "android_alarm_manager_plus/usage",
-      toReferenceAPI("android_alarm_manager"),
+    main: [
+        'overview',
+        {
+            type: 'category',
+            label: 'Android Alarm Manager Plus',
+            items: [
+                "android_alarm_manager_plus/overview",
+                "android_alarm_manager_plus/usage",
+                toReferenceAPI("android_alarm_manager"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Battery Plus',
+            items: [
+                "battery_plus/overview",
+                "battery_plus/usage",
+                toReferenceAPI("battery"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Connectivity Plus',
+            items: [
+                "connectivity_plus/overview",
+                "connectivity_plus/usage",
+                toReferenceAPI("connectivity"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Device Info Plus',
+            items: [
+                "device_info_plus/overview",
+                "device_info_plus/usage",
+                toReferenceAPI("device_info"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Network Plus',
+            items: [
+                "network_info_plus/overview",
+                "network_info_plus/usage",
+                toReferenceAPI("network_info"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Package Info Plus',
+            items: [
+                "package_info_plus/overview",
+                "package_info_plus/usage",
+                toReferenceAPI("package_info"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Sensors Plus',
+            items: [
+                "sensors_plus/overview",
+                "sensors_plus/usage",
+                toReferenceAPI("sensors"),
+            ],
+        },
+        {
+            type: 'category',
+            label: 'Share Plus',
+            items: [
+                "share_plus/overview",
+                "share_plus/usage",
+                toReferenceAPI("share"),
+            ],
+        },
     ],
-    ["Android Intent Plus"]: [
-      "android_intent_plus/overview",
-      "android_intent_plus/usage",
-      toReferenceAPI("android_intent"),
-    ],
-    ["Battery Plus"]: [
-      "battery_plus/overview",
-      "battery_plus/usage",
-      toReferenceAPI("battery"),
-    ],
-    ["Connectivity Plus"]: [
-      "connectivity_plus/overview",
-      "connectivity_plus/usage",
-      toReferenceAPI("connectivity"),
-    ],
-    ["Device Info Plus"]: [
-      "device_info_plus/overview",
-      "device_info_plus/usage",
-      toReferenceAPI("device_info"),
-    ],
-    ["Network Info Plus"]: [
-      "network_info_plus/overview",
-      "network_info_plus/usage",
-      toReferenceAPI("network_info"),
-    ],
-    ["Package Info Plus"]: [
-      "package_info_plus/overview",
-      "package_info_plus/usage",
-      toReferenceAPI("package_info"),
-    ],
-    ["Share Plus"]: [
-      "share_plus/overview",
-      "share_plus/usage",
-      toReferenceAPI("share"),
-    ],
-    ["Sensors Plus"]: [
-      "sensors_plus/overview",
-      "sensors_plus/usage",
-      toReferenceAPI("sensors"),
-    ],
-  },
 };
