e5345ea9806fd943c1e5f29cc0c02de2b6c13884
Volodymyr Buberenko <vbuberen@users.noreply.github.com>
2023-05-08 20:33:44 +0300

    Update network_info_plus.dart

--- packages/network_info_plus/network_info_plus/lib/network_info_plus.dart
+++ packages/network_info_plus/network_info_plus/lib/network_info_plus.dart
@@ -13,6 +13,7 @@ export 'package:network_info_plus_platform_interface/network_info_plus_platform_
 
 export 'src/network_info_plus_linux.dart';
 export 'src/network_info_plus_windows.dart';
+  if (dart.library.html) 'src/network_info_plus_web.dart';
 
 /// Discover network info: check WI-FI details and more.
 class NetworkInfo {
