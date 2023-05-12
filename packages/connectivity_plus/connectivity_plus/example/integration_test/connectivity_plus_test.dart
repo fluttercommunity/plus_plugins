c8f7b6342a7c51eafafae95792775505d2b52ce9
Tim Sneath <tim@sneath.org>
2023-05-12 00:03:43 -0700

    chore: Win32 dependency upgrade (#1805)

--- packages/connectivity_plus/connectivity_plus/example/integration_test/connectivity_plus_test.dart
+++ packages/connectivity_plus/connectivity_plus/example/integration_test/connectivity_plus_test.dart
@@ -1,5 +1,3 @@
-//@dart=2.9
-
 // Copyright 2019 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -13,9 +11,8 @@ import 'package:integration_test/integration_test.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
+  late Connectivity connectivity;
   group('Connectivity test driver', () {
-    Connectivity connectivity;
-
     setUpAll(() async {
       connectivity = Connectivity();
     });
