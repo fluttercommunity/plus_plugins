ced1d4d2951311c74d8023a5a852164db7954872
Sander Roest <s.roest@dalosy.com>
2023-07-04 11:01:19 +0200

    Create a method channel specifically for the integration tests, and implement the method 'createBundleAndReturn'

--- packages/android_intent_plus/example/android/app/src/main/java/io/flutter/plugins/androidintentexample/MainActivity.java
+++ packages/android_intent_plus/example/android/app/src/main/java/io/flutter/plugins/androidintentexample/MainActivity.java
@@ -2,9 +2,22 @@ package io.flutter.plugins.androidintentexample;
 
 import android.content.IntentFilter;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+import dev.fluttercommunity.plus.androidintent.Bundle.AndroidOsBundle;
+import dev.fluttercommunity.plus.androidintent.Bundle.Bundles;
+import dev.fluttercommunity.plus.androidintent.Bundle.Helpers;
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+  private static final String CHANNEL = "io.flutter.plugins.androidintentexample/integration_tests";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +26,29 @@ public class MainActivity extends FlutterActivity {
     IntentFilter filter = new IntentFilter("com.example.broadcast");
     MyBroadcastReceiver receiver = new MyBroadcastReceiver();
     registerReceiver(receiver, filter);
+  }
+
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+        .setMethodCallHandler(
+            (call, result) -> {
+
+              if ("createBundleAndReturn".equalsIgnoreCase(call.method)) {
+                try {
+                  final String listOfAndroidOsBundleAsJson = Helpers.notNullOrThrow(call.argument("extras"), "No extras provided");
+                  final List<Bundle> _androidOsBundles = AndroidOsBundle.fromJsonString(listOfAndroidOsBundleAsJson);
+                  final Bundles _bundles = Bundles.fromListOfAndroidOsBundle(_androidOsBundles);
+                  final JSONObject jsonObject = _bundles.toJson();
+                  result.success(jsonObject.toString());
+                } catch (Exception exception) {
+                  result.error("onMethodCall", exception.getMessage(), null);
+                }
+              } else {
+                result.notImplemented();
+              }
+            }
+        );
   }
 }
