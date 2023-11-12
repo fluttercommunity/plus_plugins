084a1537ec4edfac71717e5b5a92cb7f75a6bc4b
Ellet <hello@freshplatform.net>
2023-11-12 21:26:54 +0300

    Fix macOS warrning for device_info_plus

--- packages/device_info_plus/device_info_plus/macos/Classes/SystemUUID.swift
+++ packages/device_info_plus/device_info_plus/macos/Classes/SystemUUID.swift
@@ -4,7 +4,7 @@ public struct SystemUUID {
     
     public static func getSystemUUID() -> String? {
         let dev = IOServiceMatching("IOPlatformExpertDevice")
-        let platformExpert: io_service_t = IOServiceGetMatchingService(kIOMasterPortDefault, dev)
+        let platformExpert: io_service_t = IOServiceGetMatchingService(kIOMainPortDefault, dev)
         let serialNumberAsCFString = IORegistryEntryCreateCFProperty(platformExpert, kIOPlatformUUIDKey as CFString, kCFAllocatorDefault, 0)
         IOObjectRelease(platformExpert)
         let ser: CFTypeRef? = serialNumberAsCFString?.takeUnretainedValue()
