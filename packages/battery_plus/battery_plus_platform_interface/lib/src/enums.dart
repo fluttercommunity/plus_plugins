b0d8346ae0a921d7bb4c2f9accb6e953b4dbdf15
Volodymyr <v.buberenko@gmail.com>
2023-12-01 13:08:13 +0200

    feat(battery_plus): Introduce not_charging state on Linux

--- packages/battery_plus/battery_plus_platform_interface/lib/src/enums.dart
+++ packages/battery_plus/battery_plus_platform_interface/lib/src/enums.dart
@@ -7,8 +7,11 @@ enum BatteryState {
   charging,
 
   /// Device is connected to external power source, but not charging the battery.
+  ///
   /// Usually happens when device has charge limit enabled and this limit is reached.
-  /// Available on MacOS and Android platforms only.
+  /// Also, battery might be in this state if connected power source isn't powerful enough to charge the battery.
+  ///
+  /// Available on Android, MacOS and Linux platforms only.
   connectedNotCharging,
 
   /// The battery is currently losing energy.
