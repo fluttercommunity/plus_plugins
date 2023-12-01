78f44bf41a7e8349240bacae2dd70598ba22e97a
Volodymyr Buberenko <vbuberen@users.noreply.github.com>
2023-12-01 12:18:22 +0200

    feat(battery_plus)!: Introduce connected_not_charging state on MacOS (#2399)

--- packages/battery_plus/battery_plus_platform_interface/lib/src/enums.dart
+++ packages/battery_plus/battery_plus_platform_interface/lib/src/enums.dart
@@ -1,17 +1,19 @@
 /// Indicates the current battery state.
 enum BatteryState {
-  /// The battery is completely full of energy.
+  /// The battery is fully charged.
   full,
 
-  /// The battery is currently storing energy.
+  /// The battery is currently charging.
   charging,
+
+  /// Device is connected to external power source, but not charging the battery.
+  /// Usually happens when device has charge limit enabled and this limit is reached.
+  /// Available on MacOS and Android platforms only.
+  connectedNotCharging,
 
   /// The battery is currently losing energy.
   discharging,
 
-  /// The battery is not charging.
-  notCharging,
-
   /// The state of the battery is unknown.
-  unknown
+  unknown;
 }
