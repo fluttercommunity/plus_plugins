1e63d34b4071da4cac421fa0176e4c2755d054cd
Nico Mexis <nico.mexis@kabelmail.de>
2024-01-29 17:52:01 +0100

    Fix fishy registry value

--- packages/device_info_plus/device_info_plus/lib/src/device_info_plus_windows.dart
+++ packages/device_info_plus/device_info_plus/lib/src/device_info_plus_windows.dart
@@ -54,6 +54,7 @@ class DeviceInfoPlusWindowsPlugin extends DeviceInfoPlatform {
       final digitalProductIdValue =
           currentVersionKey.getValue('DigitalProductId');
       final digitalProductId = digitalProductIdValue != null
+          && digitalProductIdValue.data is Uint8List
           ? digitalProductIdValue.data as Uint8List
           : [] as Uint8List;
       final displayVersion =
