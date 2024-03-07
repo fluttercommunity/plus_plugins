3e6877d3d5d57e6355c88a96864850f754950a14
Volodymyr <v.buberenko@gmail.com>
2024-03-07 09:10:13 +0200

    feat(network_info_plus): Update min iOS target to 12

--- packages/network_info_plus/network_info_plus/example/lib/main.dart
+++ packages/network_info_plus/network_info_plus/example/lib/main.dart
@@ -27,7 +27,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
+  const MyHomePage({super.key, this.title});
 
   final String? title;
 
