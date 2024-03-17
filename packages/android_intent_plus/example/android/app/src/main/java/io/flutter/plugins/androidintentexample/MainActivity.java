fd489200a714594aad4e2eac5f0e56f43ebd751a
Volodymyr Buberenko <vbuberen@users.noreply.github.com>
2024-03-17 11:35:09 +0200

    build(android_intent_plus): Update to target and compile SDK 34 on Android (#2711)

--- packages/android_intent_plus/example/android/app/src/main/java/io/flutter/plugins/androidintentexample/MainActivity.java
+++ packages/android_intent_plus/example/android/app/src/main/java/io/flutter/plugins/androidintentexample/MainActivity.java
@@ -1,17 +1,20 @@
 package io.flutter.plugins.androidintentexample;
 
+import android.annotation.SuppressLint;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import androidx.core.content.ContextCompat;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
 
+  @SuppressLint("WrongConstant")
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     IntentFilter filter = new IntentFilter("com.example.broadcast");
     MyBroadcastReceiver receiver = new MyBroadcastReceiver();
-    registerReceiver(receiver, filter);
+    ContextCompat.registerReceiver(this, receiver, filter, ContextCompat.RECEIVER_EXPORTED);
   }
 }
