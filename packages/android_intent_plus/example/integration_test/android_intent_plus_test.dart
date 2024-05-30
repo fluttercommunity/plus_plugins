a2432132a3c3a3dfa538f8daa1c2db12f7492d10
Miguel Beltran <m@beltran.work>
2024-05-30 15:48:29 +0200

    Revert "code change to test"
    This reverts commit cd75b447bb7a5bea2ce67eee9bce92e5608a8623.

--- packages/android_intent_plus/example/integration_test/android_intent_plus_test.dart
+++ packages/android_intent_plus/example/integration_test/android_intent_plus_test.dart
@@ -64,8 +64,6 @@ void main() {
     await intent.launch();
   }, skip: !Platform.isAndroid);
 
-  // TODO REMOVE ME
-
   testWidgets('LaunchChooser should not throw', (WidgetTester tester) async {
     const intent = AndroidIntent(
       action: 'android.intent.action.SEND',
