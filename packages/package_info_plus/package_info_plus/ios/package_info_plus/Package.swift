edd2e5005751292a35684ce5fd64436566a0ad8e
charafau <gorudonu@gmail.com>
2024-08-12 01:54:48 +0900

    added spm support for packange info

---
+++ packages/package_info_plus/package_info_plus/ios/package_info_plus/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "package_info_plus",
+    platforms: [
+        .iOS("12.0"),
+    ],
+    products: [
+        .library(name: "package-info-plus", targets: ["package_info_plus"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "package_info_plus",
+            dependencies: [],
+            resources: [
+                .process("PrivacyInfo.xcprivacy"),
+            ],
+            cSettings: [
+                // TODO: Update your plugin name.
+                .headerSearchPath("include/package_info_plus")
+            ]
+        )
+    ]
+)
