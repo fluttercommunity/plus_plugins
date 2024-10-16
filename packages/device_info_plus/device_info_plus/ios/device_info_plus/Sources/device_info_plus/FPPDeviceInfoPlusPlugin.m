6a347cb106182d68329cd32827938e26bc7e7b00
Rafal Wachol <gorudonu@gmail.com>
2024-10-17 05:34:13 +0900

    feat(device_info_plus): Add Swift Package Manager support (#3167)
    Co-authored-by: Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
    Co-authored-by: Volodymyr <v.buberenko@gmail.com>

--- packages/device_info_plus/device_info_plus/ios/device_info_plus/Sources/device_info_plus/FPPDeviceInfoPlusPlugin.m
+++ packages/device_info_plus/device_info_plus/ios/device_info_plus/Sources/device_info_plus/FPPDeviceInfoPlusPlugin.m
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "FPPDeviceInfoPlusPlugin.h"
+#import "./include/device_info_plus/FPPDeviceInfoPlusPlugin.h"
 #import <sys/utsname.h>
 
 @implementation FPPDeviceInfoPlusPlugin
