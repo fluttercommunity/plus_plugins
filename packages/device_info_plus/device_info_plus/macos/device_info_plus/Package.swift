46df23023a5ba6c98edd31d5fd06bec5df40bd3b
Volodymyr Buberenko <vbuberen@users.noreply.github.com>
2024-11-07 10:38:04 +0200

    fix(device_info_plus): Update privacy manifest paths (#3347)

--- packages/device_info_plus/device_info_plus/macos/device_info_plus/Package.swift
+++ packages/device_info_plus/device_info_plus/macos/device_info_plus/Package.swift
@@ -17,7 +17,8 @@ let package = Package(
             name: "device_info_plus",
             dependencies: [],
             resources: [
-            ]
+                .process("PrivacyInfo.xcprivacy"),
+            ],
         )
     ]
 )
