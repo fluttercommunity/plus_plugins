e29eca7a1438d3fa963963b739468c5b02696d50
Volodymyr <v.buberenko@gmail.com>
2024-12-10 12:04:29 +0200

    Update forgotten tests

--- packages/device_info_plus/device_info_plus/test/model/macos_device_info_test.dart
+++ packages/device_info_plus/device_info_plus/test/model/macos_device_info_test.dart
@@ -8,8 +8,8 @@ void main() {
     group('fromMap | data', () {
       const macosDeviceInfoMap = <String, dynamic>{
         'arch': 'arch',
+        'model': 'Mac16,2',
         'modelName': 'iMac (24-inch, 2024)',
-        'modelIdentifier': 'Mac16,2',
         'activeCPUs': 4,
         'memorySize': 16,
         'cpuFrequency': 2,
@@ -25,10 +25,9 @@ void main() {
 
       test('fromMap should return $MacOsDeviceInfo with correct values', () {
         final macosDeviceInfo = MacOsDeviceInfo.fromMap(macosDeviceInfoMap);
-
         expect(macosDeviceInfo.arch, 'arch');
+        expect(macosDeviceInfo.model, 'Mac16,2');
         expect(macosDeviceInfo.modelName, 'iMac (24-inch, 2024)');
-        expect(macosDeviceInfo.modelIdentifier, 'Mac16,2');
         expect(macosDeviceInfo.activeCPUs, 4);
         expect(macosDeviceInfo.memorySize, 16);
         expect(macosDeviceInfo.cpuFrequency, 2);
