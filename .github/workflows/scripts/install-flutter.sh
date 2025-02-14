b64a01ebc6e24a9f336063352bd5506f54cd65bf
Miguel Beltran <m@beltran.work>
2025-02-14 13:27:13 +0100

    debugging why install fails

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -4,3 +4,8 @@ BRANCH=$1
 
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
 echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
+
+echo "Installed Flutter to $GITHUB_WORKSPACE/_flutter"
+
+echo "Flutter version:"
+flutter --version
