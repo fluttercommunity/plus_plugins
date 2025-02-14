deb5471310ffb21bb26ee9a0568ef01621bbde69
Miguel Beltran <m@beltran.work>
2025-02-14 13:44:31 +0100

    use flutter version instead of 'stable'

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-BRANCH=$1
+# BRANCH=$1
+# Temporal fix for https://github.com/fluttercommunity/plus_plugins/pull/3479
+# Update when https://github.com/flutter/flutter/issues/163198 has a resolution
+BRANCH="3.27.4"
 
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
-echo "$GITHUB_WORKSPACE/_flutter/bin" >> "$GITHUB_PATH"
+echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
--- .github/workflows/scripts/install-tools.sh
+++ .github/workflows/scripts/install-tools.sh
@@ -2,19 +2,8 @@
 
 set -e
 
-echo "Path: $PATH"
-ls -l "$GITHUB_WORKSPACE/_flutter/bin"
-
-echo "Flutter version:"
-flutter --version
-
 flutter config --no-analytics
-flutter pub global activate melos 6.3.2
-
-echo $HOME
-echo $GITHUB_WORKSPACE
-echo $GITHUB_PATH
-
+flutter pub global activate melos 3.0.0
 echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
 echo "$HOME/AppData/Local/Pub/Cache/bin" >> $GITHUB_PATH
 echo "$GITHUB_WORKSPACE/_flutter/.pub-cache/bin" >> $GITHUB_PATH
