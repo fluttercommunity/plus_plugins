a67e14fcf8036482d9a8cc601838477799454fff
Miguel Beltran <m@beltran.work>
2025-02-14 13:29:07 +0100

    echo path

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -3,9 +3,10 @@
 BRANCH=$1
 
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
-echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
+echo "$GITHUB_WORKSPACE/_flutter/bin" >> "$GITHUB_PATH"
 
 echo "Installed Flutter to $GITHUB_WORKSPACE/_flutter"
+echo "Path: $GITHUB_PATH"
 
 echo "Flutter version:"
 flutter --version
