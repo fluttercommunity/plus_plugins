2b4c828efd84cd627780c3ff4279a6f286d8240b
Miguel Beltran <m@beltran.work>
2025-03-07 09:47:38 +0100

    chore(device_info_plus): fix analysis issues in plugin and tests

--- packages/device_info_plus/device_info_plus/test/device_info_plus_linux_test.dart
+++ packages/device_info_plus/device_info_plus/test/device_info_plus_linux_test.dart
@@ -1,5 +1,5 @@
 @TestOn('linux')
-library device_info_plus_linux_test;
+library;
 
 import 'package:device_info_plus/src/device_info_plus_linux.dart';
 import 'package:device_info_plus_platform_interface/device_info_plus_platform_interface.dart';
