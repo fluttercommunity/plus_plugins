9b5de5311c4e53397432fcdff842ab9648c00afb
andyzhshg <andyzhshg@gmail.com>
2026-05-21 19:27:37 +0800

    fix(device_info_plus): add FlutterFramework SPM dependency

--- packages/device_info_plus/device_info_plus/ios/device_info_plus/Package.swift
+++ packages/device_info_plus/device_info_plus/ios/device_info_plus/Package.swift
@@ -11,11 +11,15 @@ let package = Package(
     products: [
         .library(name: "device-info-plus", targets: ["device_info_plus"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
+    ],
     targets: [
         .target(
             name: "device_info_plus",
-            dependencies: [],
+            dependencies: [
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ],
             resources: [
                 .process("PrivacyInfo.xcprivacy"),
             ],
