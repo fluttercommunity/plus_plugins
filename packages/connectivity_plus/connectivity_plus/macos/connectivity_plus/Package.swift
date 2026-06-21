9c064c2eaa50c13f27a6e7d2b788f57580658353
Volodymyr Buberenko <vbuberen@users.noreply.github.com>
2026-06-21 18:43:54 +0300

    feat(connectivity_plus): Updated Swift Package Manager setup for Flutter 3.44 (#3893)

--- packages/connectivity_plus/connectivity_plus/macos/connectivity_plus/Package.swift
+++ packages/connectivity_plus/connectivity_plus/macos/connectivity_plus/Package.swift
@@ -11,11 +11,15 @@ let package = Package(
     products: [
         .library(name: "connectivity-plus", targets: ["connectivity_plus"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
+    ],
     targets: [
         .target(
             name: "connectivity_plus",
-            dependencies: [],
+            dependencies: [
+                .product(name: "FlutterFramework", package: "FlutterFramework")
+            ],
             resources: [
                 .process("PrivacyInfo.xcprivacy"),
             ]
