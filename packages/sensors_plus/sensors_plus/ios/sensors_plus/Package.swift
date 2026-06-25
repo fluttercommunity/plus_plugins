bc7a6a0fcfb1bad2ffed7780a8e04bb6080fba82
Volodymyr Buberenko <vbuberen@users.noreply.github.com>
2026-06-25 08:54:07 +0300

    feat(sensors_plus): Add support of built-in Kotlin (#3902)

--- packages/sensors_plus/sensors_plus/ios/sensors_plus/Package.swift
+++ packages/sensors_plus/sensors_plus/ios/sensors_plus/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "sensors_plus",
     platforms: [
-        .iOS("12.0"),
+        .iOS("13.0"),
     ],
     products: [
         .library(name: "sensors-plus", targets: ["sensors_plus"])
